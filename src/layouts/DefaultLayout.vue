edf428d4e539076ca9f0775cf4682577e87de7e2
khoa-polls <huukhoa29@gmail.com>
2025-05-29 11:06:51 +0700

    fix(DefaultLayout):Fix websocket client declaration

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -11,7 +11,7 @@
 import Header from '@/components/layout/Header.vue'
 import { WebSocketClient } from '@/plugins/websocket'
 import { useAuthStore } from '@/stores/modules/auth'
-import { defineComponent, onMounted, onUnmounted } from 'vue'
+import { defineComponent, onMounted, onUnmounted, ref } from 'vue'
 import { useRouter } from 'vue-router'
 
 export default defineComponent({
@@ -20,7 +20,7 @@ export default defineComponent({
   setup() {
     const authStore = useAuthStore()
     const router = useRouter()
-    let wsClient: WebSocketClient | null = null
+    const wsClient = ref<WebSocketClient | null>(null)
 
     onMounted(async () => {
       if (!authStore.isAuthenticated) {
@@ -34,15 +34,14 @@ export default defineComponent({
       if (!authStore.user?.user?.id) {
         router.push('/login')
       } else {
-        wsClient = new WebSocketClient(authStore.user.user.id.toString())
-        wsClient.connect()
+        wsClient.value = new WebSocketClient(authStore.user.user.id.toString())
+        wsClient.value.connect()
       }
     })
 
     onUnmounted(() => {
-      if (wsClient) {
-        wsClient.disconnect()
-      }
+   
+      wsClient.value?.disconnect()
     })
 
     return {}

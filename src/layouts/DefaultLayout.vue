7d88d764c41192f305305bb80abf26a5c9870ad9
khoa-polls <huukhoa29@gmail.com>
2025-05-29 11:02:03 +0700

    Fix websocket client declaration and disconnect in DefaultLayout.vue

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -40,7 +40,9 @@ export default defineComponent({
     })
 
     onUnmounted(() => {
-      wsClient?.disconnect()
+      if (wsClient) {
+        wsClient.disconnect()
+      }
     })
 
     return {}
